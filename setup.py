fb02c8640b8460e85857545ad3d8e235a145429f
Dennis Piehl <piehld@gmail.com>
2024-12-09 17:36:05 -0500

    Update Azure pipelines to use latest macOS, Ubuntu, and python 3.10

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ),
     entry_points={"console_scripts": []},
     #
